2f38c6f85ca5375a67231a42e53900b1116de2a4
Reza <rz.abs.shi@gmail.com>
2018-09-08 10:48:07 +0430

    bug fixed

--- dictator/dict/sum_dict.py
+++ dictator/dict/sum_dict.py
@@ -2,6 +2,9 @@ from typing import List
 
 
 def sum_dict(data: List[dict], keys: tuple):
+    if len(data) == 0:
+        return {}
+
     relation_notation = '__'
 
     results = {}
@@ -15,17 +18,16 @@ def sum_dict(data: List[dict], keys: tuple):
             key_parts = key.split(relation_notation)
             sum_key = key_parts[-1]
 
-            data_element = data
-            result_element = results
+            for data_element in data:
+                result_element = results
 
-            for rel_key in key_parts[:-1]:
-                if rel_key not in result_element:
-                    result_element[rel_key] = {}
+                for rel_key in key_parts[:-1]:
+                    if rel_key not in result_element:
+                        result_element[rel_key] = {}
 
-                result_element = result_element[rel_key]
-                data_element = data_element.get(rel_key, {})
+                    result_element = result_element[rel_key]
+                    data_element = data_element.get(rel_key, {})
 
-            value = sum([d.get(sum_key, 0) for d in data_element])
-            result_element[sum_key] = value
+                    result_element[sum_key] = result_element.get(sum_key, 0) + data_element.get(sum_key, 0)
 
     return results
