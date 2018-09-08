94f0a685f3daa50f13cecc13a29e19ad99e705e1
Reza <rz.abs.shi@gmail.com>
2018-09-08 10:33:21 +0430

    sum_dict supports in depth relations

--- dictator/dict/sum_dict.py
+++ dictator/dict/sum_dict.py
@@ -1,14 +1,31 @@
-from typing import List, Union
+from typing import List
 
 
-def sum_dict(data: List[dict], keys: list, key_postfix: str='_sum'):
+def sum_dict(data: List[dict], keys: tuple):
+    relation_notation = '__'
 
-    result = {}
+    results = {}
 
-    for k in keys:
-        key = k + key_postfix
-        value = sum([d[k] for d in data])
+    for key in keys:
+        if relation_notation not in key:
+            value = sum([d.get(key, 0) for d in data])
+            results[key] = value
 
-        result[key] = value
+        else:
+            key_parts = key.split(relation_notation)
+            sum_key = key_parts[-1]
 
-    return result
+            data_element = data
+            result_element = results
+
+            for rel_key in key_parts[:-1]:
+                if rel_key not in result_element:
+                    result_element[rel_key] = {}
+
+                result_element = result_element[rel_key]
+                data_element = data_element.get(rel_key, {})
+
+            value = sum([d.get(sum_key, 0) for d in data_element])
+            result_element[sum_key] = value
+
+    return results
