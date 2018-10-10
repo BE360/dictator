5cdb6fdabbb3c130fef405d7e09a2dea408ec890
Reza <rz.abs.shi@gmail.com>
2018-10-10 16:31:34 +0330

    kwargs added to merge_dict

--- dictator/dict/merge_dict.py
+++ dictator/dict/merge_dict.py
@@ -1,9 +1,12 @@
 
-def merge_dict(*data_list: dict):
+def merge_dict(*args: dict, **kwargs):
     merged_data = {}
 
-    for data in data_list:
+    for data in args:
         for k, v in data.items():
             merged_data[k] = v
+
+    for data_name, data in kwargs.items():
+        merged_data[data_name] = data
 
     return merged_data
