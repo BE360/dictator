82b61e0fe7e4b29a9522ed7d29b048e3994ae688
Reza <rz.abs.shi@gmail.com>
2018-11-14 16:17:43 +0330

    list_to_dict added

--- dictator/dict/__init__.py
+++ dictator/dict/__init__.py
@@ -1,3 +1,4 @@
 from .restructure_dict import restructure_dict
 from .sum_dict import sum_dict
 from .merge_dict import merge_dict
+from .list_to_dict import list_to_dict
---
+++ dictator/dict/list_to_dict.py
@@ -0,0 +1,22 @@
+
+
+def list_to_dict(data_list: list, keys: tuple, distinct=True):
+    data_dict = {}
+
+    for data in data_list:
+        if isinstance(keys, str):
+            _key = data.get(keys)
+        else:
+            _key = tuple(data.get(k) for k in keys)
+
+        _val = data
+
+        if distinct:
+            data_dict[_key] = _val
+        else:
+            if _key not in data_dict:
+                data_dict[_key] = [_val]
+            else:
+                data_dict[_key].append(_val)
+
+    return data_dict
